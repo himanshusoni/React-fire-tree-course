2189c1dfe496f6c86aca99bd6ae8cbbfb803cf22
Himanshu Soni <himanshuson@gmail.com>
2017-03-23 00:17:32 -0500

    initial firebase configuration and test

--- src/App.js
+++ src/App.js
@@ -2,7 +2,34 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import * as firebase from 'firebase'
+
+const config = {
+  apiKey: "AIzaSyAf4BfnkoO7Em0-KoPuzW1OQW6p1eeJG9w",
+  authDomain: "react-fire-tree-course.firebaseapp.com",
+  databaseURL: "https://react-fire-tree-course.firebaseio.com",
+  storageBucket: "react-fire-tree-course.appspot.com",
+  messagingSenderId: "624055152138"
+};
+
+const db = firebase.initializeApp(config);
+
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      fbData: 'Look here to see the magic !!'
+    }
+  }
+  componentDidMount() {
+    this.globalRef = db.database().ref('data/');
+    this.globalRef.on('value', (data) => {
+      this.setState({ fbData: data })
+    });
+  }
+  componentWillUnmount() {
+    this.globalRef.off();
+  }
   render() {
     return (
       <div className="App">
@@ -12,6 +39,9 @@ class App extends Component {
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+          {JSON.stringify(this.state.fbData)}
         </p>
       </div>
     );
